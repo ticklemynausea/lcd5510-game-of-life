8f48932090704b7eefee3fb37de359c0f2d4e01c
Mário <emailoftheyear@gmail.com>
2014-10-28 03:16:10 +0000

    Tidy up a bit ;)

--- Nokia_5110_LCD_GameOfLife.ino
+++ Nokia_5110_LCD_GameOfLife.ino
@@ -18,10 +18,12 @@ igotbugs@ticklemynausea.net */
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
+/* LCD Parameters */
 #define LCD_WIDTH 84
 #define LCD_HEIGHT 48
 #define LCD_HEIGHT8 (LCD_HEIGHT >> 3)
 
+/* mapping macros */
 #define map(i, j) (i + LCD_WIDTH*j)
 #define map_x(i, j, k) (i)
 #define map_y(i, j, k) (j*8+k)
@@ -29,29 +31,41 @@ igotbugs@ticklemynausea.net */
 #define map_j(x, y) (y/8)
 #define map_k(x, y) (y%8)
 
+/* cycles */
+/* * over Matrix buffer width */
 #define for_i for (int i = 0; i < LCD_WIDTH; i++)
+/* * over Matrix buffer height */
 #define for_j for (int j = 0; j < LCD_HEIGHT8; j++)
+/* * bits over a byte */
 #define for_k for (int k = 0; k < 8; k++)
 #define for_n for (int n = 0; n < 8; n++)
-#define for_b for (int b = 0; b < 5; b++)
 
+/* * Buttons * */
+#define for_b for (int b = 0; b < 5; b++)
+/* * over screen width * */
 #define for_x for (int x = 0; x < LCD_WIDTH; x++)
+/* * over screen height * */
 #define for_y for (int y = 0; y < LCD_HEIGHT; y++)
 
-#define bit_read(matrix, i, j, k) bitRead(matrix[map(i, j)], k)
-#define bit_set(matrix, i, j, k) bitSet(matrix[map(i, j)], k)
-#define bit_clear(matrix, i, j, k) bitClear(matrix[map(i, j)], k)
+/* * read/set/clear bit k in buffer position i j * */
+#define bit_read(Matrix, i, j, k) bitRead(Matrix[map(i, j)], k)
+#define bit_set(Matrix, i, j, k) bitSet(Matrix[map(i, j)], k)
+#define bit_clear(Matrix, i, j, k) bitClear(Matrix[map(i, j)], k)
 
-#define bit_set_xy(matrix, x, y) bit_set(matrix, map_i(x, y),  map_j(x, y), map_k(x, y))
-#define bit_read_xy(matrix, x, y) bit_read(matrix, map_i(x, y),  map_j(x, y), map_k(x, y))
-#define bit_clear_xy(matrix, x, y) bit_clear(matrix, map_i(x, y),  map_j(x, y), map_k(x, y));
+/* * read/set/clear bits using screen position coordinates * */
+#define bit_set_xy(Matrix, x, y) bit_set(Matrix, map_i(x, y),  map_j(x, y), map_k(x, y))
+#define bit_read_xy(Matrix, x, y) bit_read(Matrix, map_i(x, y),  map_j(x, y), map_k(x, y))
+#define bit_clear_xy(Matrix, x, y) bit_clear(Matrix, map_i(x, y),  map_j(x, y), map_k(x, y));
 
+/* * set/unset pixel in screen using buffer coordinates) */
 #define pixel_set(i, j, k) display.drawPixel(i, j*8+k, BLACK)
 #define pixel_unset(i, j, k) display.drawPixel(i, j*8+k, WHITE)
-#define pixel_map(i, j, k) display.drawPixel(i, j*8+k, bit_read(matrix, i, j, k) == 1 ? BLACK : WHITE)
+#define pixel_map(i, j, k) display.drawPixel(i, j*8+k, bit_read(Matrix, i, j, k) == 1 ? BLACK : WHITE)
 
+/* * checks if n is in the interval [a, b] * */
 #define between(n, a, b) ((n >= a) && (n <= b))
 
+/* * neighbour directions */
 #define NORTH 0
 #define NORTHEAST 1
 #define EAST 2
@@ -61,55 +75,99 @@ igotbugs@ticklemynausea.net */
 #define WEST 6
 #define NORTHWEST 7
 
-#define RAND_RANGE 255
-
-// Initialize LCD Display
-// D7 - Serial clock out (CLK oder SCLK)
-// D6 - Serial data out (DIN)
-// D5 - Data/Command select (DC oder D/C)
-// D4 - LCD chip select (CE oder CS)
-// D3 - LCD reset (RST)
+/* Initialize LCD Display
+   D7 - Serial clock out (CLK oder SCLK)
+   D6 - Serial data out (DIN)
+   D5 - Data/Command select (DC oder D/C)
+   D4 - LCD chip select (CE oder CS)
+   D3 - LCD reset (RST) */
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
-unsigned char matrix[LCD_WIDTH * LCD_HEIGHT8] = {0};
-unsigned char matrix_new[LCD_WIDTH * LCD_HEIGHT8] = {0};
-unsigned char toggle = 0;
+/* Matrix and temporary Matrix */
+unsigned char Matrix[LCD_WIDTH * LCD_HEIGHT8] = {0};
+unsigned char Matrix_new[LCD_WIDTH * LCD_HEIGHT8] = {0};
+
+/* LED toggle */
+unsigned char LED_State = 0;
 
 /* button pins */
-unsigned char button_pins[5] = {8, 9, 10, 11, 12};
+unsigned char Buttons_Pinout[5] = {8, 9, 10, 11, 12};
 
-/* button states */
-unsigned char button_states[5] = {LOW};
+/* button states (to be used) */
+unsigned char Buttons_ButtonStates[5] = {LOW};
 
 /* button callbacks */
-void callback_button_1();
-void callback_button_2();
-void callback_button_3();
-void callback_button_4();
-void callback_button_5();
+/* * forward declarations * */
+void Buttons_CallbackButton1();
+void Buttons_CallbackButton2();
+void Buttons_CallbackButton3();
+void Buttons_CallbackButton4();
+void Buttons_CallbackButton5();
+
+/* * callback array */
 void (*button_callbacks[5])() = {
-  callback_button_1, callback_button_2, callback_button_3, callback_button_4, callback_button_5
+  Buttons_CallbackButton1, Buttons_CallbackButton2, Buttons_CallbackButton3, Buttons_CallbackButton4, Buttons_CallbackButton5
 };
 
-/*   ______            __
-    / ____/  ____     / /
-   / / __   / __ \   / /
-  / /_/ /  / /_/ /  / /___
-  \____/   \____/  /_____/ */
+/* Initialize the LED display */
+void Display_Initialize();
 
-void initialize_matrix(char limit) {
-  for_x {
-    for_y {
-      char rand = random(RAND_RANGE);
-      if (between(rand, 0, limit)) {
-        bit_set_xy(matrix, x, y);
-      }
+/* Standard "Update Display" routine: Maps the bitmap in Matrix to the LCD screen */
+void Display_Update();
+
+
+/* Matrix object: initializations */
+void Matrix_InitializeRandom();
+void Matrix_InitializePattern();
+void Matrix_InitializeBitmap();
+
+/* Implements Game of Life in using the main Matrix object */
+void Matrix_GameOfLife_dubaiss();
+
+/* Initialize LED GPIO Pin */
+void LED_Initialize();
+
+/* Toggles the standard LED on the arduino */
+void LED_Toggle();
+
+/* Reads the input from the tactile switches connected to the digital input pins in Buttons_Pinout[] */
+void Buttons_Read();
+
+/* Loop Routines */
+void (*loop_DisplayUpdate)() = Display_Update;
+void (*loop_UpdateMatrix)() = Matrix_GameOfLife_dubaiss;
+void (*loop_Led)() = LED_Toggle;
+void (*loop_Buttons)() = Buttons_Read;
+
+
+/* Data strucutre initialization & Game of Life algorithm */
+void Matrix_InitializeRandom() {
+  for_i {
+    for_j {
+      Matrix[map(i, j)] = random(256);
     }
   }
 }
 
-// implemented using dubaiss' "neighbours XXX" algorithm
-void evolve_matrix() {
+void Matrix_InitializePattern() {
+  for_i {
+    for_j {
+      if ((i + j) % 2 == 0)
+        Matrix[map(i, j)] = random(256);
+    }
+  }
+}
+
+void Matrix_InitializeBitmap() {
+  for_i {
+    for_j {
+      Matrix[map(i, j)] = random(256);
+    }
+  }
+}
+
+/* implemented using dubaiss' "neighbours XXX" algorithm */
+void Matrix_GameOfLife_dubaiss() {
   
   // new state
   unsigned char neighbour[8] = {0};
@@ -121,18 +179,18 @@ void evolve_matrix() {
   for_i { 
     for_j {
 
-      byte_cell = matrix[map(i, j)];
+      byte_cell = Matrix[map(i, j)];
 
       /* * get each bit's neighbour one byte at a time * */
       /* east and west */
       neighbour[WEST] = 0b00000000;
       if (i > 0) {
-        neighbour[WEST] = matrix[map((i - 1), j)];
+        neighbour[WEST] = Matrix[map((i - 1), j)];
       }
 
       neighbour[EAST] = 0b00000000;
       if (i < (LCD_WIDTH - 1)) {
-        neighbour[EAST] = matrix[map((i + 1), j)];
+        neighbour[EAST] = Matrix[map((i + 1), j)];
       }
       
       /* north */
@@ -140,12 +198,12 @@ void evolve_matrix() {
       neighbour[NORTHEAST] = 0b00000000;      
       neighbour[NORTHWEST] = 0b00000000;      
       if (j > 0) {
-        neighbour[NORTH] = matrix[map(i, j - 1)];
+        neighbour[NORTH] = Matrix[map(i, j - 1)];
         if (i > 0) {
-          neighbour[NORTHWEST] = matrix[map(i - 1, j - 1)];
+          neighbour[NORTHWEST] = Matrix[map(i - 1, j - 1)];
         }
         if (i < (LCD_WIDTH - 1)) {
-          neighbour[NORTHEAST] = matrix[map(i + 1, j - 1)];
+          neighbour[NORTHEAST] = Matrix[map(i + 1, j - 1)];
         }
       }
       neighbour[NORTH] = (byte_cell << 1) | ((neighbour[NORTH] & 0b10000000) >> 7);
@@ -157,12 +215,12 @@ void evolve_matrix() {
       neighbour[SOUTHEAST] = 0b00000000;      
       neighbour[SOUTHWEST] = 0b00000000;   
       if (j < (LCD_HEIGHT8 - 1)) {
-        neighbour[SOUTH] = matrix[map(i, j + 1)];
+        neighbour[SOUTH] = Matrix[map(i, j + 1)];
         if (i > 0) {
-          neighbour[SOUTHWEST] = matrix[map(i - 1, j + 1)];
+          neighbour[SOUTHWEST] = Matrix[map(i - 1, j + 1)];
         }
         if (i < (LCD_WIDTH - 1)) {
-          neighbour[SOUTHEAST] = matrix[map(i + 1, j + 1)];
+          neighbour[SOUTHEAST] = Matrix[map(i + 1, j + 1)];
         }
       }
       neighbour[SOUTH] = ((neighbour[SOUTH] & 0b00000001) << 7) | (byte_cell >> 1);
@@ -189,20 +247,39 @@ void evolve_matrix() {
         byte_cell >>= 1;
       }
       
-      matrix_new[map(i, j)] = byte_cell_new;
+      Matrix_new[map(i, j)] = byte_cell_new;
     }
   }
   
   // apply new state
   for_i {
     for_j {
-      matrix[map(i, j)] = matrix_new[map(i, j)];
+      Matrix[map(i, j)] = Matrix_new[map(i, j)];
     }
   }
 
 }
 
-void update_display() {
+void Matrix_Flip() {
+  for_i {
+    for_j {
+      Matrix[map(i, j)] = ~Matrix[map(i, j)];
+    }
+  }
+}
+
+void Display_Initialize() {
+  // initialize display
+  display.begin();
+
+  // set contrast
+  display.setContrast(60);
+  
+  // clears the screen and buffer
+  display.clearDisplay(); 
+}
+
+void Display_Update() {
 
   for_i {
     for_j {
@@ -215,104 +292,90 @@ void update_display() {
   display.display();
 }
 
-void toggle_pin() {
-  if (toggle == 1) {
+
+void LED_Initialize() {
+  pinMode(13, OUTPUT);
+}
+
+void LED_Toggle() {
+  if (LED_State == 1) {
     digitalWrite(13, HIGH); 
-    toggle = 0;
+    LED_State = 0;
   } else {
     digitalWrite(13, LOW);
-    toggle = 1;
+    LED_State = 1;
   }
 }
 
-/*    ____        __  __
-     / __ )__  __/ /_/ /_____  ____  _____
-    / __  / / / / __/ __/ __ \/ __ \/ ___/
-   / /_/ / /_/ / /_/ /_/ /_/ / / / (__  )
-  /_____/\__,_/\__/\__/\____/_/ /_/____/  */
-
-void initialize_buttons() {
+/* Buttons */
+void Buttons_Initialize() {
   for_b {
-    pinMode(button_pins[b], INPUT_PULLUP);  
+    pinMode(Buttons_Pinout[b], INPUT_PULLUP);  
   }
 }
 
-void read_buttons() {
+void Buttons_Read() {
   for_b {
-    int buttonState = digitalRead(button_pins[b]);
+    int Buttonstate = digitalRead(Buttons_Pinout[b]);
     
     // check if the pushbutton is pressed.
-    // if it is, the buttonState is HIGH:
-    if (buttonState == HIGH) {
-      digitalWrite(13, HIGH); 
-  
-    } else if (buttonState == LOW) {
+    // if it is, the Buttonstate is HIGH:
+    if (Buttonstate == HIGH) {
+      0;  
+    } else if (Buttonstate == LOW) {
       button_callbacks[b]();
-      digitalWrite(13, LOW);
     }
   }
 }
 
-void callback_button_1() {
-  initialize_matrix(RAND_RANGE / 2);
+void Buttons_CallbackButton1() {
+  Matrix_InitializeRandom();
 }
 
-void callback_button_2() {
-  initialize_matrix(RAND_RANGE * (2/3));
+void Buttons_CallbackButton2() {
+  Matrix_InitializePattern();
 }
 
-void callback_button_3() {
-  initialize_matrix(RAND_RANGE * (1/3));
+void Buttons_CallbackButton3() {
+  Matrix_InitializeBitmap();
 }
 
-void callback_button_4() {
-  delay(200);  
+void Buttons_CallbackButton4() {
+  Matrix_Flip(); 
 }
 
-void callback_button_5() {
-  delay(500);  
+void Buttons_CallbackButton5() {
+  delay(250);  
 }
 
-/* _____      __                 ___        __  ___      _
-  / ___/___  / /___  ______     ( _ )      /  |/  /___ _(_)___
-  \__ \/ _ \/ __/ / / / __ \   / __ \/|   / /|_/ / __ `/ / __ \
- ___/ /  __/ /_/ /_/ / /_/ /  / /_/  <   / /  / / /_/ / / / / /
-/____/\___/\__/\__,_/ .___/   \____/\/  /_/  /_/\__,_/_/_/ /_/
-                   /_/ */
+/* Setup & Loop */
 
 void setup()   {
 
   // random seed
   randomSeed(analogRead(0));
 
-  // initialize display
-  display.begin();
-
-  // set contrast
-  display.setContrast(60);
-  
-  // clears the screen and buffer
-  display.clearDisplay();
+  Display_Initialize();
   
   // gpio pins
-  pinMode(13, OUTPUT);
+  LED_Initialize();
 
-  // initialize buttons
-  initialize_buttons();
+  // initialize Buttons
+  Buttons_Initialize();
 
-  // initialize matrix
-  initialize_matrix(RAND_RANGE / 2);
+  // initialize Matrix
+  Matrix_InitializeRandom();
 }
 
 
 void loop() {
  
-  update_display();
+  loop_DisplayUpdate();
 
-  evolve_matrix();
+  loop_UpdateMatrix();
   
-  toggle_pin();
+  loop_Led();
 
-  read_buttons();
+  loop_Buttons();
  
 }
